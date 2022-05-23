b38985158804bb67ef4172c9ce5c8a153d0fe51f
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-05-23 14:33:24 +0900

    commit for deploy

--- client/__tests__/pages/common.test.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react";
-
-import Sign from "../../src/Pages/Sign/Sign";
-
-test("로그인 라는 글자가 포함되는가?", () => {
-  render(<Sign />);
-  const helloEl = screen.getByText(/로그인/i);
-  expect(helloEl).toBeInTheDocument();
-});
